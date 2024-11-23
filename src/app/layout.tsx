0f47fabf3940b706e2de220967c3b5064636a397
rafaelalma <rafaelalma92@gmail.com>
2024-11-23 08:28:36 +0100

    feat: add lato font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import '@/app/ui/global.css'
+import { lato } from './ui/fonts'
 
 export default function RootLayout({
   children,
@@ -7,7 +8,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="antialiased">{children}</body>
+      <body className={`${lato.className} antialiased`}>{children}</body>
     </html>
   )
 }
---
+++ src/app/ui/fonts.ts
@@ -0,0 +1,3 @@
+import { Lato } from 'next/font/google'
+
+export const lato = Lato({ subsets: ['latin'], weight: ['400', '700'] })
