cc76ee95c6e948f45e4dc782fdf6dd626856a1f8
rafaelalma <rafaelalma92@gmail.com>
2024-11-24 20:13:21 +0100

    feat: add signin with GitHub

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,19 @@
+import { Button } from '@/components/ui/button'
+import { signIn } from '../../../auth'
+
 export default function Page() {
   return (
     <>
       <h1 className="sr-only">Login</h1>
+      <form
+        action={async () => {
+          'use server'
+          await signIn('github')
+        }}
+        className="self-center"
+      >
+        <Button type="submit">Signin with GitHub</Button>
+      </form>
     </>
   )
 }
