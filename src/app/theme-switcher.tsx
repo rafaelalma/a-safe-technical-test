1ab50ad82e3c7bc409c974aa465ed442c04d006a
rafaelalma <rafaelalma92@gmail.com>
2024-11-24 09:18:52 +0100

    chores: install popover

--- src/app/theme-switcher.tsx
+++ src/app/theme-switcher.tsx
@@ -1,9 +1,23 @@
 'use client'
 
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/components/ui/popover'
 import useThemeSwitcher from './lib/use-theme-switcher'
 
 export default function ThemeSwitcher() {
   useThemeSwitcher()
 
-  return null
+  return (
+    <div className="fixed bottom-6 right-6">
+      <Popover>
+        <PopoverTrigger className="text-primary-foreground bg-primary p-2">
+          Customize
+        </PopoverTrigger>
+        <PopoverContent>Place content for the popover here.</PopoverContent>
+      </Popover>
+    </div>
+  )
 }
