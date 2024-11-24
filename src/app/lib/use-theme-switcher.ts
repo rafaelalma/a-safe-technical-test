fb0f5f27f2853c89027f6df1cc120ce4ce7007e0
rafaelalma <rafaelalma92@gmail.com>
2024-11-24 10:30:32 +0100

    feat: add theme switcher

--- src/app/lib/use-theme-switcher.ts
+++ src/app/lib/use-theme-switcher.ts
@@ -1,10 +1,57 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
+import { z } from 'zod'
+
+// TODO: useSyncExternal storage with local storage to get and set theme
 
 export default function useThemeSwitcher() {
-  // TODO: useSyncExternal storage with local storage to get and set theme color
-  // add class to html element
+  const [theme, setTheme] = useState<Theme>(retrieveTheme())
+
   useEffect(() => {
     const htmlElement = document.documentElement
-    htmlElement.setAttribute('class', 'dark violet')
-  }, [])
+
+    htmlElement.setAttribute('class', `${theme.color} ${theme.mode}`)
+
+    saveTheme(theme)
+  }, [theme])
+
+  const changeColor = (newColor: Theme['color']) => {
+    setTheme((prevTheme) => ({ ...prevTheme, color: newColor }))
+  }
+
+  const changeMode = (newMode: Theme['mode']) => {
+    setTheme((prevTheme) => ({ ...prevTheme, mode: newMode }))
+  }
+
+  return { theme, changeColor, changeMode }
+}
+
+type Theme = z.infer<typeof ThemeSchema>
+const ThemeSchema = z.object({
+  color: z.enum(['violet', 'rose', 'orange']),
+  mode: z.enum(['light', 'dark']),
+})
+
+const THEME_KEY = 'a-safe-technical-test-theme'
+
+const initialTheme: Theme = {
+  color: 'violet',
+  mode: 'dark',
+}
+
+const retrieveTheme = (): Theme => {
+  const localStorageTheme = window.localStorage.getItem(THEME_KEY)
+
+  if (!localStorageTheme) {
+    return initialTheme
+  }
+
+  const jsonParsedTheme = JSON.parse(localStorageTheme)
+
+  const parsedTheme = ThemeSchema.parse(jsonParsedTheme)
+
+  return parsedTheme
+}
+
+const saveTheme = (theme: Theme) => {
+  window.localStorage.setItem(THEME_KEY, JSON.stringify(theme))
 }
--- src/app/theme-switcher.tsx
+++ src/app/theme-switcher.tsx
@@ -10,7 +10,7 @@ import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group'
 import useThemeSwitcher from './lib/use-theme-switcher'
 
 export default function ThemeSwitcher() {
-  useThemeSwitcher()
+  const { theme, changeColor, changeMode } = useThemeSwitcher()
 
   return (
     <div className="fixed bottom-6 right-6">
@@ -22,21 +22,39 @@ export default function ThemeSwitcher() {
           <div className="flex flex-row gap-4">
             <div className="flex flex-col gap-4">
               <Label>Colors</Label>
-              <RadioGroup defaultValue="violet">
+              <RadioGroup value={theme.color}>
                 <div className="flex items-center space-x-2">
-                  <RadioGroupItem value="violet" id="violet" />
+                  <RadioGroupItem
+                    value="violet"
+                    id="violet"
+                    onClick={() => {
+                      changeColor('violet')
+                    }}
+                  />
                   <Label htmlFor="violet" className="text-violet-500">
                     Violet
                   </Label>
                 </div>
                 <div className="flex items-center space-x-2">
-                  <RadioGroupItem value="rose" id="rose" />
+                  <RadioGroupItem
+                    value="rose"
+                    id="rose"
+                    onClick={() => {
+                      changeColor('rose')
+                    }}
+                  />
                   <Label htmlFor="rose" className="text-rose-500">
                     Rose
                   </Label>
                 </div>
                 <div className="flex items-center space-x-2">
-                  <RadioGroupItem value="orange" id="orange" />
+                  <RadioGroupItem
+                    value="orange"
+                    id="orange"
+                    onClick={() => {
+                      changeColor('orange')
+                    }}
+                  />
                   <Label htmlFor="orange" className="text-orange-500">
                     Orange
                   </Label>
@@ -46,13 +64,23 @@ export default function ThemeSwitcher() {
 
             <div className="flex flex-col gap-4">
               <Label>Mode</Label>
-              <RadioGroup defaultValue="dark">
+              <RadioGroup value={theme.mode}>
                 <div className="flex items-center space-x-2">
-                  <RadioGroupItem value="light" id="light" />
+                  <RadioGroupItem
+                    value="light"
+                    id="light"
+                    onClick={() => {
+                      changeMode('light')
+                    }}
+                  />
                   <Label htmlFor="light">Light</Label>
                 </div>
                 <div className="flex items-center space-x-2">
-                  <RadioGroupItem value="dark" id="dark" />
+                  <RadioGroupItem
+                    value="dark"
+                    id="dark"
+                    onClick={() => changeMode('dark')}
+                  />
                   <Label htmlFor="dark">Dark</Label>
                 </div>
               </RadioGroup>
