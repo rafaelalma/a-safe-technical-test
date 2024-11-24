b763c379475051046cfae200adb3bdc2af5da8d3
rafaelalma <rafaelalma92@gmail.com>
2024-11-24 09:37:49 +0100

    draft: theme switcher component

--- src/app/theme-switcher.tsx
+++ src/app/theme-switcher.tsx
@@ -5,6 +5,8 @@ import {
   PopoverContent,
   PopoverTrigger,
 } from '@/components/ui/popover'
+import { Label } from '@/components/ui/label'
+import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group'
 import useThemeSwitcher from './lib/use-theme-switcher'
 
 export default function ThemeSwitcher() {
@@ -16,7 +18,47 @@ export default function ThemeSwitcher() {
         <PopoverTrigger className="text-primary-foreground bg-primary p-2">
           Customize
         </PopoverTrigger>
-        <PopoverContent>Place content for the popover here.</PopoverContent>
+        <PopoverContent>
+          <div className="flex flex-row gap-4">
+            <div className="flex flex-col gap-4">
+              <Label>Colors</Label>
+              <RadioGroup defaultValue="violet">
+                <div className="flex items-center space-x-2">
+                  <RadioGroupItem value="violet" id="violet" />
+                  <Label htmlFor="violet" className="text-violet-500">
+                    Violet
+                  </Label>
+                </div>
+                <div className="flex items-center space-x-2">
+                  <RadioGroupItem value="rose" id="rose" />
+                  <Label htmlFor="rose" className="text-rose-500">
+                    Rose
+                  </Label>
+                </div>
+                <div className="flex items-center space-x-2">
+                  <RadioGroupItem value="orange" id="orange" />
+                  <Label htmlFor="orange" className="text-orange-500">
+                    Orange
+                  </Label>
+                </div>
+              </RadioGroup>
+            </div>
+
+            <div className="flex flex-col gap-4">
+              <Label>Mode</Label>
+              <RadioGroup defaultValue="dark">
+                <div className="flex items-center space-x-2">
+                  <RadioGroupItem value="light" id="light" />
+                  <Label htmlFor="light">Light</Label>
+                </div>
+                <div className="flex items-center space-x-2">
+                  <RadioGroupItem value="dark" id="dark" />
+                  <Label htmlFor="dark">Dark</Label>
+                </div>
+              </RadioGroup>
+            </div>
+          </div>
+        </PopoverContent>
       </Popover>
     </div>
   )
